88d26a7e18e4a1de1aaa0e3a6392758ce741b3b8
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-05-20 17:40:04 -0400

    Finished History Progam Finish. Note: Could add roll history saving but im lazy so no lol.

--- rollhistoryproject/UI/uiplay.py
+++ rollhistoryproject/UI/uiplay.py
@@ -16,25 +16,32 @@ root.resizable(False, False)
 greetingtextbox = ctk.CTkLabel(root, text="Hello Welcome To the Dice Roller!", font=("Arial", 12))
 greetingtextbox.pack()
 
-#\n\n1. Roll d6.\n 2. Roll d20. \n 3. Roll History.\n 4. Quit.\n
-def roll_d6():
-    d6 = random.randint(1,6)
 
-    d6Ui.pack()
+rollnumber = 0
+
+def roll_d6():
+    global rollnumber
+    d6 = random.randint(1,6)
+    rollnumber += 1
+    d6ui = ctk.CTkLabel(historyframe, text=f"Roll {rollnumber},  D6: {d6}", font=("Arial", 12))
+    d6ui.pack()
     print(f"D6: {d6}")
-    return(d6)
+    return d6
 
 def roll_d20():
+    global rollnumber
     d20 = random.randint(1,20)
+    rollnumber += 1
+    d20ui = ctk.CTkLabel(historyframe, text=f"Roll {rollnumber},  D20: {d20}", font=("Arial", 12))
+    d20ui.pack()
     print(f"D20: {d20}")
-    d20Ui = ctk.CTkLabel(root, text=f"D20: {d20}")
-    return(d20)
+    return d20
 
 
 historyframe = ctk.CTkScrollableFrame(root)
 historyframe.place(relx=0.5, rely=0.5, anchor="center")
 
-historylabel = ctk.CTkLabel(historyframe, text="Hello Welcome To the Dice Roller!", font=("Arial", 12))
+historylabel = ctk.CTkLabel(historyframe, text="History:", font=("Arial", 12))
 historylabel.pack()
 
 
@@ -46,9 +53,6 @@ d6button.place(relx=0.5, rely=0.1, anchor="center")
 d20button = ctk.CTkButton(root, text=("Roll D20"), command=roll_d20)
 
 d20button.place(relx=0.5, rely=0.170, anchor="center")
-
-
-
 
 root.mainloop()
 
